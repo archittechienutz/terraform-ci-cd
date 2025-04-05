5b9083e53c98fa2875df878fe555ed62f8f571eb
ARCHIT PATEL <ARCHITHPATEL@GMAIL.COM>
2025-04-05 19:59:47 -0400

    Add Terraform CI/CD workflow

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Change based on region
+  ami           = "ami-0c02fb55956c7d316" # Change based on region
   instance_type = "t2.micro"
   tags = {
     Name = "Terraform-CICD-Instance"
-    }
+  }
 }
